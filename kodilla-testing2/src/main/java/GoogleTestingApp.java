5e07de78ba06c3ba07097b916fddbe667c7a94b4
JarekSoja <jarek.soja@gmail.com>
2018-06-23 20:58:41 +0200

    Selenium

---
+++ kodilla-testing2/src/main/java/GoogleTestingApp.java
@@ -0,0 +1,20 @@
+import config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class GoogleTestingApp {
+
+    public static final String SEARCHFIELD = "lst-ib";
+
+    public static void main(String[] args) {
+
+        WebDriver webDriver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+
+        webDriver.get("https://www.google.com");
+
+        WebElement searchField = webDriver.findElement(By.id(SEARCHFIELD));
+        searchField.sendKeys("olkuski");
+
+    }
+}
